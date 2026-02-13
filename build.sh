881f445aa183169886d58830af1c63805bd1afb5
carolusquintus <carlos.alejandro.rosas@walmart.com>
2026-02-13 12:31:58 -0600

    Update build.sh script with steps to generate a native executable from jar file

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-find ./src -type f -name "*.java" | xargs javac -cp ./src/ -d ./out/
-jar cvfm task-tracker-cli.jar ./src/manifest.txt -C ./out .
-java -jar task-tracker-cli.jar
+# removes all files from ./out folder
+rm -vrf ./out
+# get all and only .java files existent at ./src folder and print result to source.txt
+find ./src -type f -name "*.java" > source.txt
+# compiles all .java files obtained from source.txt and place them at ./out folder
+javac @source.txt -d ./out
+# remove source.txt
+rm ./source.txt
+# generates a .jar file from compiled files from ./out folder using manifest.txt
+jar cvfm task-tracker-cli.jar ./manifest.txt -C ./out .
+# create a native binary executable with task name, from generated jar
+native-image -jar ./task-tracker-cli.jar task
