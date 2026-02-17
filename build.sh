d9b871cf0eba482f0eb52f101c1c21ac40ba9b36
carolusquintus <carloschars@gmail.com>
2026-02-16 22:04:41 -0600

    Separate instructions from cli commands

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# removes all files from ./out folder
+# 1- removes all files from ./out folder
+# 2- get all and only .java files existent at ./src folder and print result to source.txt
+# 3- compiles all .java files obtained from source.txt and place them at ./out folder
+# 4- remove source.txt
+# 5- generates a .jar file from compiled files from ./out folder using manifest.txt
+# 6- create a native binary executable with task name, from generated jar
+
 rm -vrf ./out
-# get all and only .java files existent at ./src folder and print result to source.txt
 find ./src -type f -name "*.java" > source.txt
-# compiles all .java files obtained from source.txt and place them at ./out folder
 javac @source.txt -d ./out
-# remove source.txt
 rm ./source.txt
-# generates a .jar file from compiled files from ./out folder using manifest.txt
 jar cvfm task-tracker-cli.jar ./manifest.txt -C ./out .
-# create a native binary executable with task name, from generated jar
 native-image -jar ./task-tracker-cli.jar task
